a6b945146428e49f6e9cfd68c075144b5618fa3f
Joseph Tubridy and Lisa Jensen <joseph@>
2019-07-31 11:40:40 -0700

    add a little scaffolding to run methods on tamagotchi

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -21,9 +21,9 @@ namespace TamagotchiList.Controllers
         }
 
         [HttpPost("/tamagotchi")]
-        public ActionResult Create(string description)
+        public ActionResult Create(string description, int foodLevel, int attentionLevel, int sleepLevel)
         {
-            Tamagotchi myTamagotchi = new Tamagotchi(description);
+            Tamagotchi myTamagotchi = new Tamagotchi(description, foodLevel, attentionLevel, sleepLevel);
             return RedirectToAction("Index");
         }
 
@@ -40,6 +40,25 @@ namespace TamagotchiList.Controllers
             Tamagotchi foundTamagotchi = Tamagotchi.Find(id);
             return View(foundTamagotchi);
         }
+
+        [HttpPost("/tamagotchi/feed")]
+        public ActionResult do something that feeds 1, and subtracts from all others
+        {
+
+        }
+    [HttpPost("/tamagotchi/pet")]
+    public ActionResult do something that pets 1, and subtracts from all others
+        {
+            
+        }
+
+[HttpPost("/tamagotchi/tranq")]
+public ActionResult do something that tranqs 1, and subtracts from all others
+        {
+            
+        }
+        
+        
 
     }
 }
--- Models/Item.cs
+++ Models/Item.cs
@@ -5,12 +5,19 @@ namespace TamagotchiList.Models
     public class Tamagotchi
     {
         public string Description { get; set; }
+        public int FoodLevel { get; set; }
+        public int AttentionLevel { get; set; }
+        public int SleepLevel { get; set; }
         public int Id { get; }
         private static List<Tamagotchi> _instances = new List<Tamagotchi> { };
 
-        public Tamagotchi(string description)
+        public Tamagotchi(string description, int foodLevel, int attentionLevel, int sleepLevel)
         {
             Description = description;
+            this.FoodLevel = 11;
+            this.AttentionLevel = 11;
+            this.SleepLevel = 11;
+
             _instances.Add(this);
             Id = _instances.Count;
         }
